991bbc023f20bc0d13c8c5b89ba06655ff3058e0
USER <ihsan@ubuntu.ubuntu-domain>
2023-04-07 03:27:24 +0700

    berhasil menampilkan semua data mahasiswa

--- koneksi.js
+++ koneksi.js
@@ -1,10 +1,10 @@
-var mysql = require('mysql');
+var mysql = require('mysql2');
 
 // buat koneksi database
 const conn = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '',
+    password: 'root',
     database: 'dbrestapi'
 })
 
