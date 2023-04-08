72d6284cb0bf27909987ddf9fd0694492e7ee219
USER <ihsan@ubuntu.ubuntu-domain>
2023-04-08 23:44:17 +0700

    berhasil menampilkan nested json matakuliah

--- controller.js
+++ controller.js
@@ -3,15 +3,15 @@
 var response = require('./res');
 var connection = require('./koneksi');
 
-exports.index = function(req, res) {
+exports.index = function (req, res) {
     response.ok("Aplikasi REST API ku berjalan!", res);
 };
 
 // menampilkan semua data mahasiswa
-exports.tampilsemuamahasiswa = function(req, res) {
-    connection.query('select * from mahasiswa', function(error, rows, fields) {
-        if(error) {
-            connection.log(error);
+exports.tampilsemuamahasiswa = function (req, res) {
+    connection.query('select * from mahasiswa', function (error, rows, fields) {
+        if (error) {
+            console.log(error);
         } else {
             response.ok(rows, res)
         }
@@ -19,61 +19,80 @@ exports.tampilsemuamahasiswa = function(req, res) {
 }
 
 // menampilkan data mahasiswa berdasarkan id
-exports.tampilberdasarkanid = function(req, res) {
+exports.tampilberdasarkanid = function (req, res) {
     let id = req.params.id;
     connection.query('select * from mahasiswa where id_mahasiswa = ?', [id],
-        function(error, rows, fields) {
-            if(error) {
+        function (error, rows, fields) {
+            if (error) {
                 connection.log(error);
             } else {
                 response.ok(rows, res)
             }
-    });
+        });
 }
 
 // menambahkan data mahasiswa
-exports.tambahmahasiswa = function(req, res) {
+exports.tambahmahasiswa = function (req, res) {
     var nim = req.body.nim;
     var nama = req.body.nama;
     var jurusan = req.body.jurusan;
 
     connection.query('insert into mahasiswa (nim, nama, jurusan) values(?,?,?)',
-        [nim, nama, jurusan], function(error, rows, fields) {
-            if(error){
+        [nim, nama, jurusan], function (error, rows, fields) {
+            if (error) {
                 console.log(error);
             } else {
                 response.ok('Berhasil menambahkan data', res);
             }
-    });
+        });
 }
 
 // mengubah data berdasarkan id
-exports.ubahmahasiswa = function(req, res) {
+exports.ubahmahasiswa = function (req, res) {
     var id = req.body.id_mahasiswa;
     var nim = req.body.nim;
     var nama = req.body.nama;
     var jurusan = req.body.jurusan;
 
     connection.query('update mahasiswa set nim=?, nama=?, jurusan=? where id_mahasiswa=?',
-        [nim, nama, jurusan, id], function(error, rows, fields) {
-            if(error) {
+        [nim, nama, jurusan, id], function (error, rows, fields) {
+            if (error) {
                 console.log(error);
             } else {
                 response.ok('Berhasil mengubah data', res);
             }
-    });
+        });
 }
 
 // menghapus data berdasarkan id
-exports.hapusmahasiswa =  function(req, res) {
+exports.hapusmahasiswa = function (req, res) {
     var id = req.body.id_mahasiswa;
 
     connection.query('delete from mahasiswa where id_mahasiswa=?', [id],
-        function(error, rows, fields) {
-            if(error) {
+        function (error, rows, fields) {
+            if (error) {
                 console.log(error);
             } else {
                 response.ok('Berhasil menghapus data', res);
             }
-    });
+        });
+}
+
+// menampilkan matakuliah group
+exports.tampilgroupmatakuliah = function(req, res) {
+    connection.query(
+        'select mahasiswa.id_mahasiswa, mahasiswa.nim, mahasiswa.nama, mahasiswa.jurusan, ' + 
+        'matakuliah.matakuliah, matakuliah.sks from krs join matakuliah join mahasiswa ' +
+        'where krs.id_matakuliah = matakuliah.id_matakuliah ' +
+        'and krs.id_mahasiswa = mahasiswa.id_mahasiswa order by mahasiswa.id_mahasiswa',
+        function (error, rows, fields) {
+            if (error) {
+                console.log(error);
+            } else {
+                response.oknested(rows, res)
+            }
+        }
+    );
+
+
 }
